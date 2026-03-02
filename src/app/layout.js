035bfd618420a504bae7cd3b995260db27d60dcc
josuesamorim <josuedesouzaamorim@gmail.com>
2026-03-02 08:23:44 -0300

    fix: imports

--- src/app/layout.js
+++ src/app/layout.js
@@ -67,12 +67,6 @@ export const metadata = {
   },
 };
 
-import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/next";
-import { GoogleTagManager } from "@next/third-parties/google";
 
 
 /**
